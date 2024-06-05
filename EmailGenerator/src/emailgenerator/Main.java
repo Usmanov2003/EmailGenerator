76de45d9769401faf7af574bd4e22cc73773980c
usmanov Khabibullo o'gli <140377792+Usmanov2003@users.noreply.github.com>
2024-06-05 12:39:10 +0600

    Add files via upload

---
+++ EmailGenerator/src/emailgenerator/Main.java
@@ -0,0 +1,8 @@
+package emailgenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailBackProgram email = new  EmailBackProgram("Someione","Someone");
+        System.out.println(email.showInfo());
+    }
+}
